9faebd389755691f4592e9e37d3915cdfca803aa
Maram Shubbar <maram20052@gmail.com>
2026-08-31 10:10:53 +0300

    setup navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import './Navbar.css'
+
+function NavBar(){
+    return (
+    <nav id="top-navbar">
+        <a href="/">Home</a>
+        <a href="/about-us">About Us</a>
+        <a href="/money-pit">Investment Opportunities</a>
+        <a href="/the-fine-print">Terms of Service</a>
+    </nav>
+    )
+}
+
+
+
+export default NavBar
